a784377dbdd9a48180df31b52368239c4b8e563b
Adil Rehman <51363290+AdilRehm@users.noreply.github.com>
2020-09-15 09:23:49 +0500

    Add files via upload

---
+++ js/scriptindex.js
@@ -0,0 +1,13 @@
+var named=["Adil","Rehman","Jason","Jaket","Italian","Iron"];
+	for (var x =0; x <named.length; x++) {
+		var firstLetter=named[x].charAt(0).toLowerCase();
+		if (firstLetter=='j') {
+			console.log("GoodBye "+named[x]);
+		}
+		else
+		{
+			console.log("Hello " +named[x]);
+		}
+	}
+	// Other Task time of Easier
+	console.log("Easier Assignment start from there");
